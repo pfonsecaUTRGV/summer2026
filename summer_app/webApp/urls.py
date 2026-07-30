75ce2622be195d5abb69ab3786830359dee27c0d
Pedro Fonseca <pedro@Pedros-MacBook-Air.local>
2026-07-30 11:18:14 -0500

    Added the register form

--- summer_app/webApp/urls.py
+++ summer_app/webApp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include 
-from . import views
+from django.contrib.auth import views as auth_views
+from . import views 
 
 urlpatterns = [
 	path('',views.home, name ='home'),
-	path('about/',views.about, name ='about')
+	path('about/',views.about, name ='about'),
+	path('register/',views.register, name ='register'),
+	#path('login/',auth_views.LoginView.as_view(template_name=login.html), name ='login'),
+	#path('logout/',auth_views.LogoutView.as_view(), name ='logout')
 ]
--- summer_app/webApp/views.py
+++ summer_app/webApp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages 
+
 
 # Create your views here.
 
@@ -8,3 +11,16 @@ def home(request):
 
 def about(request): 
 	return render(request,'about.html',{})
+
+def register(request):
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			messages.success(request,"Account created succesfully")
+			print("New user was created")
+			#return redirect("login")
+	else:
+		form = UserCreationForm()
+
+	return render(request, "register.html",{"form":form})
