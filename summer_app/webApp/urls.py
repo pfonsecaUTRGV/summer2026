d7e0d9b87fa16baec72fcb0b0b2b1dc537c7c7a9
Pedro Fonseca <pedro@Pedros-MacBook-Air.local>
2026-08-03 10:05:09 -0500

    Create login path and website

--- summer_app/webApp/urls.py
+++ summer_app/webApp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 	path('',views.home, name ='home'),
 	path('about/',views.about, name ='about'),
 	path('register/',views.register, name ='register'),
-	#path('login/',auth_views.LoginView.as_view(template_name=login.html), name ='login'),
-	#path('logout/',auth_views.LogoutView.as_view(), name ='logout')
+	path('login/',auth_views.LoginView.as_view(template_name="login.html"), name ='login'),
+	path('logout/',auth_views.LogoutView.as_view(), name ='logout')
 ]
