c5da20e1ea96a4b5ecc4c65e4cb5ff94fc0be76e
Pedro Fonseca <pedro@Pedros-MacBook-Air.local>
2026-08-03 10:20:27 -0500

    update

--- summer_app/webApp/urls.py
+++ summer_app/webApp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 	path('about/',views.about, name ='about'),
 	path('register/',views.register, name ='register'),
 	path('login/',auth_views.LoginView.as_view(template_name="login.html"), name ='login'),
-	path('logout/',auth_views.LogoutView.as_view(), name ='logout')
+	path('logout/',auth_views.LogoutView.as_view(), name ='logout'),
+	path("profile/",views.profile,name="profile"),
 ]
